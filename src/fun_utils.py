b3cab6438c4a05aaf59f72e6c9e57ea77b385381
MatteoCasti <111187799+MatteoCasti@users.noreply.github.com>
2023-10-20 11:46:23 +0200

    Commit load_mnist_data

--- src/fun_utils.py
+++ src/fun_utils.py
@@ -1,5 +1,7 @@
+import pandas as pd
 from pandas import read_csv
 import numpy as np
+from sklearn.metrics import pairwise_distances
 
 
 def load_data(filename):
@@ -32,3 +34,14 @@ def split_data(x, y, tr_fraction=0.5):
 
     """
     pass
+def load_mnist_data(filename, n_samples=None):
+    """This function returns MNIST handwritten digits and labels as ndarrays."""
+    data = pd.read_csv(filename)
+    data = np.array(data)  # cast pandas dataframe to numpy array
+    if n_samples is not None:  # only returning the first n_samples
+        data = data[:n_samples, :]
+    y = data[:,0]
+    x = data[:,1:] / 255.0
+    return x, y
+
+
