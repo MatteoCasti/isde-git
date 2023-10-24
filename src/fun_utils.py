6bdd109bd0e839eb162ff7fd1f83b6c39c5ea573
MatteoCasti <111187799+MatteoCasti@users.noreply.github.com>
2023-10-24 19:13:36 +0200

    update fun_utils

--- src/fun_utils.py
+++ src/fun_utils.py
@@ -1,29 +1,17 @@
+import pandas as pd
 from pandas import read_csv
 import numpy as np
 
 
-def load_data(filename):
-    """
-    Load data from a csv file
-
-    Parameters
-    ----------
-    filename : string
-        Filename to be loaded.
-
-    Returns
-    -------
-    X : ndarray
-        the data matrix.
-
-    y : ndarray
-        the labels of each sample.
-    """
-    data = read_csv(filename)
-    z = np.array(data)
-    y = z[:, 0]
-    X = z[:, 1:]
-    return X, y
+def load_data(filename, n_samples=None):
+    """This function returns MNIST handwritten digits and labels as ndarrays."""
+    data = pd.read_csv(filename)
+    data = np.array(data)  # cast pandas dataframe to numpy array
+    if n_samples is not None:  # only returning the first n_samples
+        data = data[:n_samples, :]
+    y = data[:,0]
+    x = data[:,1:] / 255.0
+    return x, y
 
 
 def split_data(X, y, tr_fraction=0.5):
