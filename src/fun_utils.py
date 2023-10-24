55afcef1e3d8060f9d8d6b1ca1b07771185c546e
MatteoCasti <111187799+MatteoCasti@users.noreply.github.com>
2023-10-24 18:27:30 +0200

    changes

--- src/fun_utils.py
+++ src/fun_utils.py
@@ -4,29 +4,6 @@ import numpy as np
 from sklearn.metrics import pairwise_distances
 
 
-def load_data(filename):
-    """
-    Load data from a csv file
-
-    Parameters
-    ----------
-    filename : string
-        Filename to be loaded.
-
-    Returns
-    -------
-    X : ndarray
-        the data matrix.
-
-    y : ndarray
-        the labels of each sample.
-    """
-    data = read_csv(filename)
-    z = np.array(data)
-    y = z[:, 0]
-    X = z[:, 1:]
-    return X, y
-
 
 def split_data(x, y, tr_fraction=0.5):
     """
