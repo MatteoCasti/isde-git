7435b60c95f09cc1a32a8ce8f3ca2154e8933b40
MatteoCasti <111187799+MatteoCasti@users.noreply.github.com>
2023-10-24 19:02:04 +0200

    cambio branch

--- src/fun_utils.py
+++ src/fun_utils.py
@@ -3,29 +3,6 @@ from pandas import read_csv
 import numpy as np
 
 
-def load_data(filename):
-    """
-    Load data from a csv file
-
-    Parameters
-    ----------
-    filename : string
-        Filename to be loaded.
-
-    Returns
-    -------
-    X : ndarray
-        the data matrix.
-
-    y : ndarray
-        the labels of each sample.
-    """
-    data = read_csv(filename)
-    z = np.array(data)
-    y = z[:, 0]
-    X = z[:, 1:]
-    return X, y
-
 
 def load_data(filename, n_samples=None):
     """This function returns MNIST handwritten digits and labels as ndarrays."""
