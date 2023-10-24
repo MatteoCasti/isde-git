7754f96aa2a672262193ae85a5fcf06a42238257
MatteoCasti <111187799+MatteoCasti@users.noreply.github.com>
2023-10-24 18:47:17 +0200

    changes

--- src/fun_utils.py
+++ src/fun_utils.py
@@ -11,14 +11,5 @@ def split_data(x, y, tr_fraction=0.5):
 
     """
     pass
-def load_data(filename, n_samples=None):
-    """This function returns MNIST handwritten digits and labels as ndarrays."""
-    data = pd.read_csv(filename)
-    data = np.array(data)  # cast pandas dataframe to numpy array
-    if n_samples is not None:  # only returning the first n_samples
-        data = data[:n_samples, :]
-    y = data[:,0]
-    x = data[:,1:] / 255.0
-    return x, y
 
 
